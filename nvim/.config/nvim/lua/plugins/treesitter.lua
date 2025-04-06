fb345aad95dd965cb2d16c057f1fc1de55f9147f
Ryan Storo <rs@pop-os.localdomain>
2025-04-06 13:01:38 -0400

    gdscript added to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "go", "javascript", "vim", "c", "vimdoc", "python", "c_sharp", "markdown", "markdown_inline" },
+      ensure_installed = { "lua", "go", "javascript", "vim", "c", "vimdoc", "python", "c_sharp", "markdown", "markdown_inline", "gdscript" },
       highlight = { enable = true },
       indent = { enable = true },
     })
